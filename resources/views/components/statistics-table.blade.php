da7784eb38c70bf749b92a67c3f933d94720be2e
Zeizvant <oxanashvilizezva@gmail.com>
2023-05-02 09:13:38 +0400

    style: add custom scrollbar to table

--- resources/views/components/statistics-table.blade.php
+++ resources/views/components/statistics-table.blade.php
@@ -1,21 +1,25 @@
 @props(['confirmed', 'recovered', 'deaths', 'countries'])
 <table class="md:w-full table-auto box-border md:relative absolute w-screen left-0 md:left-auto">
-    <x-table-header></x-table-header>
-    <tbody class="text-xs md:text-sm font-normal">
-        <tr class="h-14">
-            <td class="pl-2 md:pl-10">{{ __('landing.worldwide') }}</td>
-            <td>{{ number_format($confirmed) }}</td>
-            <td>{{ number_format($deaths) }}</td>
-            <td>{{ number_format($recovered) }}</td>
-        </tr>
-        @foreach($countries as $country)
-            <tr class="h-14">
-                <td class="pl-2 md:pl-10">{{ $country->countryName->name }}</td>
-                <td>{{ number_format($country->confirmed) }}</td>
-                <td>{{ number_format($country->deaths) }}</td>
-                <td>{{ number_format($country->recovered) }}</td>
-            </tr>
-        @endforeach
 
+    <x-table-header></x-table-header>
+
+
+    <tbody class="text-xs md:text-sm font-normal" >
+    <tr class="h-14">
+        <td class="pl-2 md:pl-10">{{ __('landing.worldwide') }}</td>
+        <td>{{ number_format($confirmed) }}</td>
+        <td>{{ number_format($deaths) }}</td>
+        <td>{{ number_format($recovered) }}</td>
+    </tr>
+    @foreach($countries as $country)
+        <tr class="h-14">
+            <td class="pl-2 md:pl-10">{{ $country->countryName->name }}</td>
+            <td>{{ number_format($country->confirmed) }}</td>
+            <td>{{ number_format($country->deaths) }}</td>
+            <td>{{ number_format($country->recovered) }}</td>
+        </tr>
+    @endforeach
     </tbody>
+
+
 </table>
