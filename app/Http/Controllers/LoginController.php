e271d02ea821d80af7e085bcf52ecfb0edad0017
Zeizvant <oxanashvilizezva@gmail.com>
2023-05-08 20:13:10 +0400

    feat: add login functionality

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+
+class LoginController extends Controller
+{
+	public function show(): View
+	{
+		return view('login');
+	}
+
+	public function login(LoginRequest $request): RedirectResponse
+	{
+		if (auth()->attempt(['username' => $request->username, 'password' => $request->password])) {
+			return redirect()->route('index');
+		}
+		return back();
+	}
+}
